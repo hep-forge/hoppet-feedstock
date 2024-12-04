f5415454e46c648c0e0f2471aca8c401cf8b0a37
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2024-12-04 11:18:06 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/bash
 
-./configure --prefix=${PREFIX}
+FFLAGS="-std=legacy" ./configure --prefix=${PREFIX}
 
 make -j$(nproc)
 make check
 
 make install
 make install-mod
+
+rm ${PREFIX}/lib/libhoppet_v1.a
