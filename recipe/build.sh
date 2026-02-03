6cffc87051985c35d98e9ebeb69bec5a6e35862e
Marco Meyer-Conde　　　  メイヤーマルコ <37673095+meiyasan@users.noreply.github.com>
2026-02-03 11:03:41 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build  build -j$(nproc)
 cmake --install build
 
-rm ${PREFIX}/lib/libhoppet*.a
-ctest --test-dir build
+#rm ${PREFIX}/lib/libhoppet*.a
+ctest --test-dir build --rerun-failed --output-on-failure
